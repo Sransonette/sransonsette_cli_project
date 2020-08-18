65ab560ebc9b66c35223cb6f8cc88269c7be63bb
Sean <Sransonette@gmail.com>
2020-08-18 17:28:17 -0500

    initial setup

---
+++ lib/sransonette_cli_project.rb
@@ -0,0 +1,6 @@
+require "sransonette_cli_project/version"
+
+module SransonetteCliProject
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/sransonette_cli_project/version.rb
@@ -0,0 +1,3 @@
+module SransonetteCliProject
+  VERSION = "0.1.0"
+end
